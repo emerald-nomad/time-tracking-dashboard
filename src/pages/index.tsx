139b459f0ff0f5a883cd844925944694eb2ab7e6
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-12-02 08:45:50 -0600

    fixing build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,11 @@ const Home: NextPage = () => {
 
       <div className="attribution">
         Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
           Frontend Mentor
         </a>
         . Coded by <a href="#">Jalen Greene</a>.
